7f508514bf1a5e7ede40015d1caacc153a2b45ee
DanRin9 <da.rinconr12@uniandes.edu.co>
2025-10-15 18:36:33 -0500

    no se hacer commmit en eclipse

--- src/dpoo/proyecto/consola/ConsolaMasterTicket.java
+++ src/dpoo/proyecto/consola/ConsolaMasterTicket.java
@@ -2,41 +2,75 @@ package dpoo.proyecto.consola;
 
 import dpoo.proyecto.app.MasterTicket;
 import dpoo.proyecto.tiquetes.Tiquete;
+import dpoo.proyecto.usuarios.Usuario;
 
 public class ConsolaMasterTicket extends ConsolaBasica {
 	
 	private MasterTicket sistemaBoleteria;
 	
-	private void menuLogin() {
-		String menu = "1 - Login y contraseña\n2 - Crear usuario";
-		System.out.println(menu);
-	}
 	
-	private void correrAplicacion() {
+	private Usuario logInYAuth() {
 		
 		try {
 			
 			sistemaBoleteria = new MasterTicket<Tiquete>();
 			
 		} catch (Exception e) {
+			e.printStackTrace();
 			
 		}
 		
 		boolean running = true;
 		
+		String menu = "1 - Login y contraseña\n2 - Crear usuario";
+		
+		System.out.println(menu);
+		
+		String opcionLogIn = pedirCadena("Seleccione la opción: ");
+		
+		if (opcionLogIn.equals("1")) {
+			String logIn = pedirCadena("Log In");
+			String contrasena = pedirCadena("Contraseña");
+			
+		}else {
+			String newLogIn = pedirCadena("Igrese un nombre de usuario");
+			String newContrasena = pedirCadena("Ingrese una contraseña");
+		}
+		
+		
+		
 		while (running) {
 			
-			menuLogin();
-			pedirCadena("Seleccione la opción");
 			
+			
+			
+		}
+		return null;
+		
+	}
+	private void menuUsuario() {
+		try {
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private void menuAdmin() {
+		
+		try {
+			
+		}catch (Exception e) {
+			e.printStackTrace();
 		}
 		
 	}
 	
+	
 	private static void main(String[] args) {
 		
 		ConsolaMasterTicket c = new ConsolaMasterTicket();
-		c.correrAplicacion();
+		c.logIn();
+		
 		
 	}
 
