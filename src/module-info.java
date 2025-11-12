d7a64b4a1f85bddca5dd95864468b09efffa5374
DanRin9 <da.rinconr12@uniandes.edu.co>
2025-11-12 15:10:11 -0500

    a

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,10 @@
-/**
- * 
- */
-/**
- * 
- */
-module ProyectoDPOO1 {
+open module masterticket.app {
     requires org.json;
+
+    exports dpoo.proyecto.app;
+    exports dpoo.proyecto.consola;
+    exports dpoo.proyecto.eventos;
+    exports dpoo.proyecto.tiquetes;
+    exports dpoo.proyecto.usuarios;
+    exports persistencia;
 }
