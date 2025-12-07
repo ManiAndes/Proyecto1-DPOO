79b7169eb27d829e63776a815e07b320494ac1d2
DanRin9 <da.rinconr12@uniandes.edu.co>
2025-12-06 22:40:49 -0500

    FINAL

--- src/dpoo/proyecto/gui/usuario/UsuarioDashboardPanel.java
+++ src/dpoo/proyecto/gui/usuario/UsuarioDashboardPanel.java
@@ -22,6 +22,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.border.EmptyBorder;
